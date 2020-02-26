7c091b00f5d6033027c9cb7b24f6eaf4e622fb4d
stianst <stianst@gmail.com>
2020-02-26 17:30:05 +0100

    Fixes

--- quarkus-keycloak-admin-client/src/main/java/org/acme/ExampleResource.java
+++ quarkus-keycloak-admin-client/src/main/java/org/acme/ExampleResource.java
@@ -1,5 +1,6 @@
 package org.acme;
 
+import org.jboss.logging.Logger;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.KeycloakBuilder;
 
@@ -11,9 +12,13 @@ import javax.ws.rs.core.MediaType;
 @Path("/hello")
 public class ExampleResource {
 
+    private static final Logger LOG = Logger.getLogger(ExampleResource.class);
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
+        LOG.info("Hello invoked");
+
         Keycloak keycloak = KeycloakBuilder.builder().serverUrl("http://localhost:8080/auth").realm("master").clientId("admin-cli").username("admin").password("admin").build();
         return keycloak.realm("master").toRepresentation().getRealm();
     }
--- quarkus-keycloak-admin-client/src/test/java/org/acme/ExampleResourceTest.java
+++ quarkus-keycloak-admin-client/src/test/java/org/acme/ExampleResourceTest.java
@@ -12,7 +12,7 @@ public class ExampleResourceTest {
     @Test
     public void testHelloEndpoint() {
         given()
-          .when().get("/realms")
+          .when().get("/hello")
           .then()
              .statusCode(200)
              .body(is("master"));
