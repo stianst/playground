07ff3f8901e48048d9eb3692806a57ef1208bee2
stianst <stianst@gmail.com>
2021-04-14 19:47:42 +0200

    Cleanip

--- resteasy-client-issue/src/main/java/org/keycloak/example/basicauth/MyRestApp.java
+++
@@ -1,8 +0,0 @@
-package org.keycloak.example.basicauth;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/rest")
-public class MyRestApp extends Application {
-}
