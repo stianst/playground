cacdcba2d2f52b556d10c1c4d68b7c04fbf93c43
stianst <stianst@gmail.com>
2023-02-22 08:02:57 +0100

    More

--- kc-production/nginx-proxy/start.sh
+++ kc-production/nginx-proxy/start.sh
@@ -2,5 +2,12 @@
 
 cd $(readlink -f $0 | xargs dirname)
 
-sudo podman build -t example-proxy .
+CONF_FILE=$1
+
+if [ "$1" == "" ] || [ ! -f "$1" ]; then
+    echo "usage: start.sh <conf_file>"
+    exit 1
+fi
+
+sudo podman build --build-arg conf_file=$CONF_FILE -t example-proxy .
 sudo podman run --network=host example-proxy
