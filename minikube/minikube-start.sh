1ee5af9625c84269d1487e772b4b6f1f6497ffb5
stianst <stianst@gmail.com>
2023-09-21 13:36:26 +0200

    Stuff

--- minikube/minikube-start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-minikube start \
-    --extra-config=apiserver.authorization-mode=RBAC \
-    --extra-config=apiserver.oidc-issuer-url=https://living-bluebird-credible.ngrok-free.app/realms/kube \
-    --extra-config=apiserver.oidc-username-claim=email \
-    --extra-config=apiserver.oidc-groups-claim=groups \
-    --extra-config=apiserver.oidc-client-id=kubectl
