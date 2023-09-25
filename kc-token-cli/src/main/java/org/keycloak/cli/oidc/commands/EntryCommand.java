1f009a424c89d74640391efb305669fb11aa7e35
stianst <stianst@gmail.com>
2023-09-25 16:17:44 +0200

    Added support for kubectl

--- kc-token-cli/src/main/java/org/keycloak/cli/oidc/commands/EntryCommand.java
+++ kc-token-cli/src/main/java/org/keycloak/cli/oidc/commands/EntryCommand.java
@@ -2,14 +2,12 @@ package org.keycloak.cli.oidc.commands;
 
 import io.quarkus.picocli.runtime.annotations.TopCommand;
 import org.keycloak.cli.oidc.commands.config.ConfigCommand;
-import org.keycloak.cli.oidc.kubectl.KubectlCommand;
 import picocli.CommandLine;
 
 @TopCommand
 @CommandLine.Command(mixinStandardHelpOptions = true, subcommands = {
         TokenCommand.class,
-        ConfigCommand.class,
-        KubectlCommand.class
+        ConfigCommand.class
 })
 public class EntryCommand {
 }
--- kc-token-cli/src/main/java/org/keycloak/cli/oidc/commands/TokenCommand.java
+++ kc-token-cli/src/main/java/org/keycloak/cli/oidc/commands/TokenCommand.java
@@ -1,12 +1,15 @@
 package org.keycloak.cli.oidc.commands;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.keycloak.cli.oidc.Output;
 import org.keycloak.cli.oidc.config.ConfigException;
 import org.keycloak.cli.oidc.config.ConfigHandler;
 import org.keycloak.cli.oidc.config.Context;
+import org.keycloak.cli.oidc.kubectl.ExecCredentialRepresentation;
 import org.keycloak.cli.oidc.oidc.OpenIDClient;
 import org.keycloak.cli.oidc.oidc.TokenParser;
 import org.keycloak.cli.oidc.oidc.exceptions.OpenIDException;
+import org.keycloak.cli.oidc.oidc.representations.JWT;
 import picocli.CommandLine;
 
 @CommandLine.Command(name = "token")
@@ -14,18 +17,38 @@ public class TokenCommand implements Runnable {
 
     @CommandLine.Option(names = {"-c", "--context"}, description = "Context to use")
     String contextName;
-    @CommandLine.Option(names = {"--type"}, description = "Token type to return", defaultValue = "access")
+    @CommandLine.Option(names = {"--type"}, description = "Token type to return")
     String tokenType;
     @CommandLine.Option(names = {"--decode"}, description = "Decode token", defaultValue = "false")
     boolean decode;
     @CommandLine.Option(names = {"--offline"}, description = "Offline mode", defaultValue = "false")
     boolean offline;
+    @CommandLine.Option(names = {"--kubectl"}, description = "Kubectl mode", defaultValue = "false")
+    boolean kubectl;
 
     @Override
     public void run() {
+        if (!kubectl && System.getenv().containsKey("KUBERNETES_EXEC_INFO")) {
+            kubectl = true;
+        }
+
+        if (tokenType == null) {
+            tokenType = kubectl ? "id" : "access";
+        }
+
         try {
-            String token = getToken();
-            if (decode) {
+            String token = getToken(tokenType);
+
+            if (kubectl) {
+                JWT jwt = TokenParser.parse(token).getJWT();
+
+                ExecCredentialRepresentation execCredential = new ExecCredentialRepresentation();
+                execCredential.getStatus().setToken(token);
+                // TODO execCredential.getStatus().setExpirationTimestamp();
+
+                ObjectMapper objectMapper = new ObjectMapper();
+                System.out.println(objectMapper.writeValueAsString(execCredential));
+            } else if (decode) {
                 String decoded = TokenParser.parse(token).decoded();
                 Output.println(decoded);
             } else {
@@ -36,7 +59,7 @@ public class TokenCommand implements Runnable {
         }
     }
 
-    public String getToken() throws OpenIDException, ConfigException {
+    private String getToken(String tokenType) throws OpenIDException, ConfigException {
         ConfigHandler configHandler = ConfigHandler.get();
         Context context = contextName != null ? configHandler.getContext(contextName) : configHandler.getCurrentContext();
         OpenIDClient openIDClient = OpenIDClient.create(configHandler, context);
