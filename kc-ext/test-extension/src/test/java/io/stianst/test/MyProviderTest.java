16156dbcc990902d5a4e8a13fcad33e5b26a39d1
stianst <stianst@gmail.com>
2026-02-06 09:10:43 +0100

    WIP

--- kc-ext/test-extension/src/test/java/io/stianst/test/MyProviderTest.java
+++ kc-ext/test-extension/src/test/java/io/stianst/test/MyProviderTest.java
@@ -1,16 +1,22 @@
 package io.stianst.test;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.keycloak.testframework.annotations.InjectRealm;
 import org.keycloak.testframework.annotations.KeycloakIntegrationTest;
+import org.keycloak.testframework.realm.ManagedRealm;
 import org.keycloak.testframework.server.KeycloakServerConfig;
 import org.keycloak.testframework.server.KeycloakServerConfigBuilder;
 
 @KeycloakIntegrationTest(config = MyProviderTest.MyServerConfig.class)
 public class MyProviderTest {
 
+    @InjectRealm
+    ManagedRealm myrealm;
+
     @Test
     public void myTest() {
-
+        Assertions.assertNotNull(myrealm.admin().toRepresentation());
     }
 
     public static class MyServerConfig implements KeycloakServerConfig {
